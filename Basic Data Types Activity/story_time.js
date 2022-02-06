710dd95acbc2e63cb7ac43ba3cfaccb02c57d464
afinney049 <98277100+afinney049@users.noreply.github.com>
2022-02-06 01:04:10 -0500

    new file2

---
+++ Basic Data Types Activity/story_time.js
@@ -0,0 +1,39 @@
+// TODO: Create 4-String variables to introduce develop your story.
+// YOUR CODE GOES HERE
+var meds = 'Bandage';
+var dr = 'Water';
+var al = 'Rubbing Alcohol';
+var str = 'Stretcher';
+
+// TODO: Create 3-String variables to set the time period of your story or discuss other number elements.
+// YOUR CODE GOES HERE
+let time = 10
+const occurence = 5
+let sunrise = 7 
+
+
+// TODO: Create 1-Array variable to show a collection of items your character might have.
+// YOUR CODE GOES HERE
+const findings = ['Fingerprints','Jacket','Liquid','Toxin']
+
+
+// TODO: Create 1-Boolean variable to demonstrate a true or false scenario.
+// YOUR CODE GOES HERE
+let isMurder = true
+
+
+// TODO: Print your story to the console.
+// YOUR CODE GOES HERE
+console.log('Brought out on a ' + str + ', ' + 'a man was found unconscious with bubbles foaming in his mouth and a ' + meds +' on his left wrist.')
+console.log('Upon closer inspection, it was found that the man was tricked into thinking that ' + al + ' inside of a glass was ' + dr + '.')
+console.log('Due to such, the police have found this case to have been a murder. ')
+console.log("They brought out one of their best detectives to solve this case with him finding the exact time of the man's reaction: " + occurence + ' P.M.')
+console.log('He has also found the time that the man had consumed the ' + findings[2] + ', at exactly ' + time + ' A.M.')
+console.log('This ensures that whoever the man was with, exactly ' + sunrise + ' hours before his reaction, is the culprit.')
+console.log('This evidence also proves that the Liquid was not just Rubbing Alcohol, but instead a deadly ' + findings[3] + ', showing the theory of a murder scenario being ' + isMurder +'.' )
+console.log("There were two people in the house at the time of consumption: the man's brother and his best friend. ")
+console.log('After some more thorough investigation, the detective has found the brothers ' + findings[1] + ' inside of the home.')
+console.log("At first the detective had believed it was the man's brother, but then he took a closer look.")
+console.log("He had found the best friend's " + findings[0] + " on his brother's jacket!")
+console.log('Noting that the best friend tried to frame the brother for his actions, he was, in the end, prosecuted for his crimes.')
+console.log('The End!')
